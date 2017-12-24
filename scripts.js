ac62c2667d70585270d3f608d3b9ba672582a54f
Matthew-Shea7 <31706592+Matthew-Shea7@users.noreply.github.com>
2017-12-24 16:06:30 -0500

    Nav controls

--- scripts.js
+++ scripts.js
@@ -1,3 +1,30 @@
-function hide() {
-    document.getElementById("nav").style.display = "none";
+function hideUP() {
+    document.getElementById("navUP").style.display = "none";
+    document.getElementById("navShowUP").style.display = "block";
+}
+
+function hidePRE() {
+    document.getElementById("navPRE").style.display = "none";
+}
+
+function hideINFO() {
+    document.getElementById("navINFO").style.display = "none";
+}
+
+function hideMARS() {
+    document.getElementById("navMARS").style.display = "none";
+}
+
+function hideTEAM() {
+    document.getElementById("navTEAM").style.display = "none";
+}
+
+function hideABT() {
+    document.getElementById("navABT").style.display = "none";
+}
+
+
+function showUP() {
+    document.getElementById("navUP").style.display = "block";
+    document.getElementById("navShowUP").style.display = "none";
 }
