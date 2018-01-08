d9e7e9b8ed58228448379f6e04817ca7ea769e45
Matthew-Shea7 <31706592+Matthew-Shea7@users.noreply.github.com>
2018-01-08 15:06:42 -0500

    js for team

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-function hide() {
-    document.getElementById("nav").style.display = "none";
-}
+$(document).ready(function(){
+    $("#ElonMusk").click(function(){
+        $("#ElonMuskcard").addClass("teamcardShow");
+        $("#ElonMuskcard").removeClass("teamcardHide");
+        $("#GwynneShotcard").removeClass("teamcardShow");
+        $("#JamesHendercard").removeClass("teamcardShow");
+        $("#TimHughescard").removeClass("teamcardShow");
+    });
