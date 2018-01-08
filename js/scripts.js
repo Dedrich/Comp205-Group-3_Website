0321f61d8a2553cc7bc20b998d5aec047f26a274
Derek Bancroft <31705796+Dedrich@users.noreply.github.com>
2018-01-08 15:06:23 -0500

    infoUpdate2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
 function hide() {
     document.getElementById("nav").style.display = "none";
 }
+
+
+
+
+
+
+
+
+
+
+
+/* Elon Musk Show Card */
+$(document).ready(function(){
+    $(".elonMusk").click(function(){
+        $(".elonCard").addClass("teamCardCenter");
+        $(".rosie").removeClass("teamCardCenter");
+        $(".rosie").removeClass("teamCardCenter");
+        $(".rosie").removeClass("teamCardCenter");
+        $(".rosie").removeClass("teamCardCenter");
+    });
+    
+/* Rosie Musk Show Card */
+$(document).ready(function(){
+    $(".rosie").click(function(){
+        $(".rosie").addClass("teamCardCenter");
+        $(".elonMusk").removeClass("teamCardCenter");
+        $(".rosie").removeClass("teamCardCenter");
+        $(".rosie").removeClass("teamCardCenter");
+        $(".rosie").removeClass("teamCardCenter");
+    });
